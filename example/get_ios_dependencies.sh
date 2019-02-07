d4176233e55fa826dfb529053209fd923ebadee9
Eoin Landy <el111@yahoo.com>
2019-02-07 22:35:12 +0000

    1.0.0

--- example/get_ios_dependencies.sh
+++ example/get_ios_dependencies.sh
@@ -10,12 +10,12 @@ wget https://github.com/tuarua/Swift-IOS-ANE/releases/download/$FreSwiftVersion/
 unzip -u -o ios_dependencies.zip
 rm ios_dependencies.zip
 
-#wget https://github.com/tuarua/Swift-IOS-ANE/releases/download/$FreSwiftVersion/AIRSDK_patch.zip
-#unzip -u -o AIRSDK_patch.zip
-#rm AIRSDK_patch.zip
+wget https://github.com/tuarua/Swift-IOS-ANE/releases/download/$FreSwiftVersion/AIRSDK_patch.zip
+unzip -u -o AIRSDK_patch.zip
+rm AIRSDK_patch.zip
 
-#wget https://github.com/tuarua/Sharea-ANE/releases/download/$AneVersion/ios_dependencies.zip
-#unzip -u -o ios_dependencies.zip
-#rm ios_dependencies.zip
+wget https://github.com/tuarua/Sharea-ANE/releases/download/$AneVersion/ios_dependencies.zip
+unzip -u -o ios_dependencies.zip
+rm ios_dependencies.zip
 
-#wget -O ../native_extension/ane/ShareANE.ane https://github.com/tuarua/Share-ANE/releases/download/$AneVersion/ShareANE.ane?raw=true
+wget -O ../native_extension/ane/ShareANE.ane https://github.com/tuarua/Share-ANE/releases/download/$AneVersion/ShareANE.ane?raw=true
