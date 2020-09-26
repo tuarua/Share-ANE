0cf6c02f7ad4fbbb958ac7dd6805b71fbe8f28c9
Eoin Landy <info@tuarua.co.uk>
2020-09-26 11:32:23 +0100

    use air-tools

--- example/get_android_dependencies.ps1
+++
@@ -1,9 +0,0 @@
-$AneVersion = "1.5.0"
-$FreKotlinVersion = "1.40.0"
-$SupportV4Version = "1.0.0"
-
-$currentDir = (Get-Item -Path ".\" -Verbose).FullName
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest -Uri https://github.com/tuarua/Share-ANE/releases/download/$AneVersion/ShareANE.ane?raw=true -OutFile "$currentDir\..\native_extension\ane\ShareANE.ane"
-Invoke-WebRequest -OutFile "$currentDir\android_dependencies\androidx.legacy.legacy-support-v4-$SupportV4Version.ane" -Uri https://github.com/tuarua/Android-ANE-Dependencies/blob/master/anes/support/androidx.legacy.legacy-support-v4-$SupportV4Version.ane?raw=true
-Invoke-WebRequest -OutFile "$currentDir\android_dependencies\com.tuarua.frekotlin-$FreKotlinVersion.ane" -Uri https://github.com/tuarua/Android-ANE-Dependencies/blob/master/anes/kotlin/com.tuarua.frekotlin-$FreKotlinVersion.ane?raw=true
